c494922a8fec81e49242473b91b107e13898fd1c
hkalbasi <hamidrezakalbasi@protonmail.com>
2026-06-26 15:13:35 +0330

    Fix CI.

--- co2_test_harness/src/compiler.rs
+++ co2_test_harness/src/compiler.rs
@@ -112,7 +112,7 @@ pub fn build_compilers(root: &Path, coverage: bool) -> Result<()> {
 
 fn ensure_compiler_links(bin_dir: &Path) -> Result<()> {
     let multicall = bin_dir.join(exe_name("co2-multicall"));
-    for applet in ["co2rustc", "co2rustdoc", "co2cc", "co2cargo", "co2miri"] {
+    for applet in ["co2rustc", "co2rustdoc", "co2cc", "co2cargo", "co2miri", "co2fmt"] {
         ensure_link(&multicall, &bin_dir.join(exe_name(applet)))?;
     }
     Ok(())
